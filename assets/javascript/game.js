8b9f9c6d82a5416472ef0cae0a5f18b006fff190
Casey <cecolby11@users.noreply.github.com>
2017-02-09 18:27:00 -0600

    writing pseudocode and steps

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+// computer picks word randomly
+  // pick a theme
+  // make an array of words that fit the theme
+  // computer randomly selects a word from the array
+
+// user guesses a letter
+  // array of possible letter guesses, make sure they picked an acceptable key or else tell them to guess a-z
+   // tell game to pay attention to the keyup
+  // save the key pressed as a variable
+
+// check if letter in word. yay! or boo.
+   // if then logic to see if it's in the word... 
+      // look for methods to do this. can we check if a char is in a string? or break the string into an array and check if it's matching element in array?
+
+// display letters guessed, update after each guess
+  // if they got it wrong, add their letter to this array 
+  // update what's displayed on the screen
+
+// display progress on the word, update after each guess
+  // if they got it right, add their letter to this array 
+  // update what's displayed on the screen
+
+// display number of guesses remaining, update after each guess
+  // counter starts at 12? 
+  // decrease by 1 after each guess
+  // update what's displayed on the screen
+
+// display # wins! if word is guessed in time, this goes up
+    
