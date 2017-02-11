a0ea9da1d4969487eb1098af23d6c1f0ea977c4e
Casey <cecolby11@users.noreply.github.com>
2017-02-10 19:57:39 -0600

    implementing basic game function in console, no page display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,12 @@
 var lettersGuessed = [];
 // array to hold blank spaces or letters for the word in question
 var wordProgressDisplay = [];
+// var for number of guesses remaining. starts at 12. 
+var guessesRemaining = 12;
+//var to hold number of wins! starts at 0. 
+var winCounter = 0; 
 // array of words that fit the theme
 var wordOptions = ["Buddy", "Ella", "Duke", "Miles"];
-
-//gameplay - Computer
-
 // computer randomly selects word from wordOptions, store in var
 var computerChoice = wordOptions[Math.floor(Math.random() * wordOptions.length)];
 // guesses will be lowercase, make word match
@@ -21,55 +22,77 @@ for(var i = 0; i < computerChoice.length; i++) {
 //use join to separate with spaces and without commas in display
 alert("ok here's your word: " + wordProgressDisplay.join(" "));
 
-//gameplay - User
+//gameplay - User guesses letter; tell game to pay attention to keyup event
 
-// user guesses letter; tell game to pay attention to the keyup event
-document.onkeyup= function(e) {
+document.onkeyup = function(e) {
   // save key pressed as variable
   var playerGuess = e.key;
   console.log("player guess: " + playerGuess);
+  //call helper functions 
+  checkGuess(playerGuess);
+  winLoseWatcher();
+}
 
-  // check if letter in word
-  // if yes, then...
+//helper functions 
+
+checkGuess = function(playerGuess) {
   if(computerChoice.includes(playerGuess)){
-    // 1. find its index in the word
-    for(var i = 0; i < computerChoice.length; i++) {
-      if(playerGuess === computerChoice.charAt(i)) {
-        console.log("letter at index: " + i);
-        //2. insert the letter guessed at that index in the wordProgressDisplay 
-          //use: splice(index, how many to be removed, items to add)
-        wordProgressDisplay.splice(i, 1, playerGuess);
-        alert("here's what you've got now: " + wordProgressDisplay.join(" "));
-        //letters guessed shouldn't be updated: 
-        console.log("letters guessed: " + lettersGuessed);
-        //if we've found the letter and updated, don't break from the for loop because it could be in the word multiple times!! 
-      }
-    }
+    findIndexInWord(playerGuess);
   } 
   //if no, then add it to the "letters guessed array" 
   else {
     console.log("nope not in this word");
-      lettersGuessed.push(playerGuess);
-      console.log("letters guessed: " + lettersGuessed);
-    }
+    updateLettersGuessed(playerGuess);
+    updateGuessesRemaining();
+  }
+}
 
+findIndexInWord = function(letter) {
+  for(var i = 0; i < computerChoice.length; i++) {
+    if(letter === computerChoice.charAt(i)) {
+      // insert @ index in wordProgressDisplay. 
+      // don't update letters guessed or guesses remaining.
+      updateWordProgressDisplay(letter, i);
+    }
+    // even if we've found the letter, don't break from for-loop because it could be in the word multiple times!! 
+  }
+}
+
+updateWordProgressDisplay = function(letter, index) {
+  //use: splice(index, how many to be removed, items to add)
+  wordProgressDisplay.splice(index, 1, letter);
+  alert("here's what you've got now: " + wordProgressDisplay.join(" "));
+  //letters guessed shouldn't be updated: 
+}
+
+updateLettersGuessed = function(letter) {
+  lettersGuessed.push(letter);
+  console.log("letters guessed: " + lettersGuessed);
+}
+
+updateGuessesRemaining = function() {
+  guessesRemaining--;
+  console.log("guesses remaining: " + guessesRemaining);
+}
+
+winLoseWatcher = function() {
+  //check if WIN (word is totally guessed (no blanks left))
+  if(!(wordProgressDisplay.includes("__"))){
+    winCounter++;
+    console.log("YOU WIN! number of wins: " + winCounter);
+  } 
+  //check if LOSS (they haven't won and guesses remaining reaches 0)
+  else if(guessesRemaining === 0) {
+    console.log("YOU LOSE!");
+  }
 }
 
 
-// display progress on the word, update after each guess
-  // if they got it right, add their letter to this array 
-  // update what's displayed on the screen
+// TODO: 
 
-// display number of guesses remaining, update after each guess
-  // counter starts at 12? 
-  // decrease by 1 after each guess
-  // update what's displayed on the screen
+// new game after win or loss declared
 
-// display # wins! if word is guessed in time, this goes up
-  
-
-
-//DO THESE LATER: 
+// DO THESE LATER: 
 
 // array of possible letter guesses, make sure they picked an acceptable key or else tell them to guess a-z 
 
@@ -77,6 +100,8 @@ document.onkeyup= function(e) {
 
 // pick a theme - update array with new words if necessary 
 
-// display lettersGuessed on screen, update what's displayed after each guess. 
+// display word progress, lettersGuessed, and guessesRemaining on the screen, update what's displayed for these 3 after each guess
 
-//if they've already guessed a letter, alert them or don't let them keep guessing it! 
+// display winCounter on thes creen and update what's displayed for this after each win 
+
+// if they've already guessed a letter, alert them or don't let them keep guessing it! (shouldn't make number of guesses keep decreasing)
